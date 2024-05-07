3280b78f6fa03cd7b6099e2a04ff16c68324468a
Filipe Vaz <filipecraveiravaz@gmail.com>
2024-05-07 18:53:25 +0100

    putting routes on routes file

--- mainApp.js
+++ mainApp.js
@@ -3,94 +3,13 @@ const app = express();
 const fs = require('fs');
 const path = require('path');
 const PORT = 5602;
-// const routes = require('./routes/routes');
+const routes = require('./routes/routes');
 
-// app.use('/', routes);
 app.use(express.static(__dirname + '/views/'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.set("view engine", "ejs");
 app.get('/favicon.ico', (req, res) => res.status(204));
-
-const root= './data';
-const urlsHistory = [];
-
-app.get('/data', (req, res) =>{
-  var directoryPath = path.resolve(root); 
-
-  urlsHistory.push(directoryPath);
-
-
-  fs.readdir(directoryPath, (err, files) => {
-    if (err) {
-      console.error('Erro ao ler diretório:', err);
-      return;
-    }
-
-    var pattern = '.';
-    const folders = files.filter((str) => {return !str.startsWith(pattern)});
-
-    console.log('Pastas existentes:', folders);
-    console.log(directoryPath);
-    console.log(urlsHistory);
-    console.log(req.originalUrl)
-
-    res.render('index', {folders: folders, dir:req.url});
-  });
-});
-
-app.get('/anterior', (req, res) =>{
-
-  const dir = req.params.diretorio || '';
-  
-  var lastUrl = urlsHistory.slice(-1).toString();
-  urlsHistory.pop(lastUrl)
-
-  var directoryPath = path.dirname(lastUrl);
-
-  fs.readdir(directoryPath, (err, files) => {
-    if (err) {
-      console.error('Erro ao ler diretório:', err);
-      return;
-    }
-    
-    var pattern = '.';
-    const folders = files.filter((str) => {return !str.startsWith(pattern)});
-    
-    console.log('Pastas existentes:', folders);
-    console.log(directoryPath)
-    console.log(urlsHistory);
-
-    res.render('index', {folders: folders, dir:dir});
-    });
-});
-
-app.get('/:diretorio?/', (req, res) =>{
-  
-  const dir = req.params.diretorio || '';
-  var lastUrl = urlsHistory.slice(-1).toString();
-
-  var directoryPath = path.join(lastUrl, dir); 
-  urlsHistory.push(directoryPath)
-
-
-  fs.readdir(directoryPath, (err, files) => {
-    if (err) {
-      console.error('Erro ao ler diretório:', err);
-      return;
-    }
-
-    var pattern = '.';
-    const folders = files.filter((str) => {return !str.startsWith(pattern)});
-
-    console.log('Pastas existentes:', folders);
-    console.log(directoryPath)
-    console.log(urlsHistory);
-    console.log(req.originalUrl)
-
-    res.render('index', {folders: folders, dir:dir});
-    });
-});
-
+app.use('/', routes);
 
 app.listen(PORT, () => {
   console.log("Listening on port 5602");
--- routes/routes.js
+++ routes/routes.js
@@ -2,11 +2,85 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
+
+const root= './data';
+const urlsHistory = [];
+
+router.get('/', (req, res) =>{
+  var directoryPath = path.resolve(root); 
+
+  urlsHistory.push(directoryPath);
   
-  router.get("/", (req, res) => {
-    console.log('GET/root');
-    res.render('index', {folders: folders});
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.error('Erro ao ler diretório:', err);
+      return;
+    }
+
+    var pattern = '.';
+    const folders = files.filter((str) => {return !str.startsWith(pattern)});
+
+    console.log('Pastas existentes:', folders);
+    console.log(directoryPath);
+    console.log(urlsHistory);
+    console.log(req.originalUrl)
+
+    res.render('index', {folders: folders, dir:req.url});
   });
+});
+
+router.get('/anterior', (req, res) =>{
+
+  const dir = directoryPath;
   
+  var lastUrl = urlsHistory.slice(-1).toString();
+  urlsHistory.pop(lastUrl)
+
+  var directoryPath = path.dirname(lastUrl);
+
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.error('Erro ao ler diretório:', err);
+      return;
+    }
+    
+    var pattern = '.';
+    const folders = files.filter((str) => {return !str.startsWith(pattern)});
+    
+    console.log('Pastas existentes:', folders);
+    console.log(directoryPath)
+    console.log(urlsHistory);
+
+    res.render('index', {folders: folders, dir:dir});
+    });
+});
+
+router.get('/:diretorio?/', (req, res) =>{
+  
+  const dir = req.params.diretorio || '';
+  var lastUrl = urlsHistory.slice(-1).toString();
+
+  var directoryPath = path.join(lastUrl, dir); 
+  urlsHistory.push(directoryPath)
+
+
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.error('Erro ao ler diretório:', err);
+      return;
+    }
+
+    var pattern = '.';
+    const folders = files.filter((str) => {return !str.startsWith(pattern)});
+
+    console.log('Pastas existentes:', folders);
+    console.log(directoryPath)
+    console.log(urlsHistory);
+    console.log(req.originalUrl)
+
+    res.render('index', {folders: folders, dir:dir});
+    });
+});
+
 
 module.exports = router; 
