f92905a2fba4b15cae4ec0c52c51148eea2f14d9
Filipe Vaz <filipecraveiravaz@gmail.com>
2024-05-09 16:30:40 +0100

    showing current directory when returning to previous directory | message to empty directory

--- routes/routes.js
+++ routes/routes.js
@@ -3,13 +3,19 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 
-const root= './data';
-const urlsHistory = [];
+const root= 'data';
+var urlsHistory = [ ];
 
 router.get('/', (req, res) =>{
+
   var directoryPath = path.resolve(root); 
 
   urlsHistory.push(directoryPath);
+
+  var urlSplit = directoryPath.split("\\");
+  var dir = urlSplit.slice(-1).toString();
+
+  console.log("url split: ", dir)
   
   fs.readdir(directoryPath, (err, files) => {
     if (err) {
@@ -19,25 +25,34 @@ router.get('/', (req, res) =>{
 
     var pattern = '.';
     const folders = files.filter((str) => {return !str.startsWith(pattern)});
+
+    if (folders.length == 0){
+      var emptyDirMsg = "Diretorio vazio!";
+    }
 
     console.log('Pastas existentes:', folders);
     console.log(directoryPath);
     console.log(urlsHistory);
     console.log(req.originalUrl)
 
-    res.render('index', {folders: folders, dir:req.url});
+    res.render('index', {folders: folders, dir:dir, emptyDirMsg:emptyDirMsg});
   });
 });
 
 router.get('/anterior', (req, res) =>{
 
-  const dir = directoryPath;
-  
+  //Get last url
   var lastUrl = urlsHistory.slice(-1).toString();
   urlsHistory.pop(lastUrl)
-
+  
+  //sets last url to be shown
   var directoryPath = path.dirname(lastUrl);
+  
+  const urlSplit = directoryPath.split("\\");
+  var dir = urlSplit.slice(-1).toString();
 
+  console.log("url split: ", urlSplit)
+  console.log("typeof directorypath: ", urlSplit[-1], typeof(dir))
   fs.readdir(directoryPath, (err, files) => {
     if (err) {
       console.error('Erro ao ler diretório:', err);
@@ -47,20 +62,23 @@ router.get('/anterior', (req, res) =>{
     var pattern = '.';
     const folders = files.filter((str) => {return !str.startsWith(pattern)});
     
+    if (folders.length == 0){
+      var emptyDirMsg = "Diretorio vazio!";
+    }
+    
     console.log('Pastas existentes:', folders);
     console.log(directoryPath)
     console.log(urlsHistory);
 
-    res.render('index', {folders: folders, dir:dir});
+    res.render('index', {folders: folders, dir:dir, emptyDirMsg:emptyDirMsg});
     });
 });
 
 router.get('/:diretorio?/', (req, res) =>{
-  
-  const dir = req.params.diretorio || '';
-  var lastUrl = urlsHistory.slice(-1).toString();
+  const newDir = req.params.diretorio || ''
 
-  var directoryPath = path.join(lastUrl, dir); 
+  var lastUrl = urlsHistory.slice(-1).toString();
+  var directoryPath = path.join(lastUrl, newDir); 
   urlsHistory.push(directoryPath)
 
 
@@ -72,13 +90,17 @@ router.get('/:diretorio?/', (req, res) =>{
 
     var pattern = '.';
     const folders = files.filter((str) => {return !str.startsWith(pattern)});
-
+    
+    if (folders.length == 0){
+      var emptyDirMsg = "Diretorio vazio!";
+    }
+    
     console.log('Pastas existentes:', folders);
     console.log(directoryPath)
     console.log(urlsHistory);
     console.log(req.originalUrl)
 
-    res.render('index', {folders: folders, dir:dir});
+    res.render('index', {folders: folders, dir:newDir, emptyDirMsg:emptyDirMsg});
     });
 });
 
