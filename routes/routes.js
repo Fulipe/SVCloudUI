dd190a9d2b4681f5c5c42fde29f9b6dca10d19da
Filipe Vaz <filipecraveiravaz@gmail.com>
2024-08-09 17:34:18 +0100

    Navegação por subdiretorios e página de visualizacao de ficheiros

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { url } = require('inspector');
 const router = express.Router();
 const fs = require('fs').promises;
 const path = require('path');
@@ -9,26 +8,37 @@ const root= path.resolve('data');
 const urlsHistory = []
 
 router.get('*', async (req,res) =>{
-    const directoryPath = path.join(root, req.path); //isto é o "handler" das routes
-    urlsHistory.push(directoryPath)
+  
+    if(req.path === '/'){
+      var directoryPath = path.join(root); //isto é o "handler" das routes
+      urlsHistory.push(directoryPath)
+    } else {
+      var lastUrl = urlsHistory.slice(-1).toString();
+      var directoryPath = path.join(lastUrl, req.path);
+      urlsHistory.push(directoryPath);
+      
+    }
 
     console.log("diretório" + directoryPath)
     console.log("req.path: "+ req.path)
-    console.log("current: "+ urlsHistory)
+    console.log("current: ", urlsHistory)
     console.log()
     try{
         const stat = await fs.stat(directoryPath);
 
         if (stat.isDirectory()){
-            const items = await fs.readdir(directoryPath, {withFileTypes: true});
-            const formattedItems = items
-            .filter(item => !item.name.startsWith('.'))
-            // .map(item => {path.join(req.path, item.name);});
-            const parentPath = req.path === '/' ? '' : path.join(req.path, '..')
-            const emptyDirMsg = "Diretorio vazio!";
+          const items = await fs.readdir(directoryPath, {withFileTypes: true});
+          const formattedItems = items.filter(item => !item.name.startsWith('.'))
+          const checkDir = items.filter(item=> item.isDirectory);
+          
+          // .map(item => {path.join(req.path, item.name);});
+          const parentPath = req.path === '/' ? '' : path.join(req.path, '..')
+          const emptyDirMsg = "Diretorio vazio!";
+            
+          console.log(items)
+          console.log(checkDir)
 
-            res.render('index', {items, formattedItems, caminho:req.path, anterior:parentPath, emptyDirMsg});
-
+          res.render('index', {checkDir, formattedItems, caminho:req.path, anterior:parentPath, emptyDirMsg});
         } else {
             const parentPath = req.path === '/' ? '' : path.join(req.path, '..')
             const content = await fs.readFile(directoryPath, 'utf-8');
