035ea14ef596522f61ed76c39f5ac2acde2f4af0
Filipe Vaz <filipecraveiravaz@gmail.com>
2024-08-14 14:52:02 +0100

    Distinção entre diretorios e ficheiros

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { type } = require('os');
 const router = express.Router();
 const fs = require('fs').promises;
 const path = require('path');
@@ -23,21 +22,18 @@ router.get('*', async (req,res) =>{
     console.log("diretório" + directoryPath)
     console.log("req.path: "+ decodedReq)
     console.log("current: ", urlsHistory)
-    console.log()
     try{
         const stat = await fs.stat(directoryPath);
 
         if (stat.isDirectory()){
           const items = await fs.readdir(directoryPath, {withFileTypes: true});
-          const formattedItems = items.filter(item => !item.name.startsWith('.'))
-          const checkDir = items.filter(item=> item.isDirectory);
+          const formattedItems = items.filter(item => !item.name.startsWith('.'));
+          const checkDir = formattedItems.filter(item=> item.isDirectory());
           
           // .map(item => {path.join(req.path, item.name);});
           const parentPath = req.path === '/' ? '' : path.join(req.path, '..')
           const emptyDirMsg = "Diretorio vazio!";
-          console.log(items)
-          console.log(checkDir)
-
+          
           res.render('index', {checkDir, formattedItems:formattedItems, caminho:decodedReq, anterior:parentPath, emptyDirMsg});
         } else {
             const parentPath = req.path === '/' ? '' : path.join(req.path, '..')
@@ -52,103 +48,5 @@ router.get('*', async (req,res) =>{
         res.status(500).send('Erro no acesso ao arquivo ou diretorio.');
     }
 });
-
-
-// router.get('/', (req, res) =>{
-
-//   var directoryPath = path.resolve(root); 
-
-//   urlsHistory.push(directoryPath);
-
-//   //tem de ser "/" porque em Linux os diretórios são separados por "/" ao contrario de Windows que é "\\"
-//   var urlSplit = directoryPath.split("/");
-//   var dir = urlSplit.slice(-1).toString();
-
-//   console.log("url split: ", dir)
-  
-//   fs.readdir(directoryPath, (err, files) => {
-//     if (err) {
-//       console.error('Erro ao ler diretório:', err);
-//       return;
-//     }
-
-//     // const folders = files.filter(file =>{fs.statSync(path.join(directoryPath, file)).isDirectory()}); -> apenas mostra diretorios
-//     const folders = files.filter((str) => {return !str.startsWith('.')});
-//     const parentPath = req.path === '/' ? '' : path.join(req.path, '..');
-
-//     const dirs = files.filter(directory =>{fs.statSync(path.join(directoryPath, directory)).isDirectory()});
-//     const archives = files.filter(arch =>{fs.statSync(path.join(directoryPath, arch)).isFile()});
-//     var emptyDirMsg = "Diretorio vazio!";
-
-//     console.log('Pastas existentes:', folders);
-//     console.log(directoryPath);
-//     console.log(urlsHistory);
-//     console.log(req.originalUrl)
-
-//     res.render('index', {folders: folders, dir:dir, emptyDirMsg, dirs:dirs, archives:archives, parentPath});
-//   });
-// });
-
-// // router.get('/anterior', (req, res) =>{
-
-// //   //Get last url
-// //   var lastUrl = urlsHistory.slice(-1).toString();
-// //   urlsHistory.pop(lastUrl)
-  
-// //   //sets last url to be shown
-// //   var directoryPath = path.dirname(lastUrl);
-  
-// //   //tem de ser "/" porque em Linux os diretórios são separados por "/" ao contrario de Windows que é "\\"
-// //   const urlSplit = directoryPath.split("/");
-// //   var dir = urlSplit.slice(-1).toString();
-
-// //   console.log("url split: ", urlSplit)
-// //   fs.readdir(directoryPath, (err, files) => {
-// //     if (err) {
-// //       console.error('Erro ao ler diretório:', err);
-// //       return;
-// //     }
-    
-// //     const folders = files.filter((str) => {return !str.startsWith('.')});
-
-// //     var emptyDirMsg = "Diretorio vazio!";
-    
-// //     console.log('Pastas existentes:', folders);
-// //     console.log(directoryPath)
-// //     console.log(urlsHistory);
-
-// //     res.render('index', {folders: folders, dir:dir, emptyDirMsg});
-// //     });
-// // });
-
-// router.get('/:diretorio?/', (req, res) =>{
-//   const newDir = req.params.diretorio || ''
-
-//   var lastUrl = urlsHistory.slice(-1).toString();
-//   var directoryPath = path.join(lastUrl, newDir); 
-//   urlsHistory.push(directoryPath)
-
-
-//   fs.readdir(directoryPath, (err, files) => {
-//     if (err) {
-//       console.error('Erro ao ler diretório:', err);
-//       return;
-//     }
-
-//     const folders = files.filter((str) => {return !str.startsWith('.')});
-
-//     const emptyDirMsg = "Diretorio vazio!";
-    
-//     const parentPath = req.path === '/' ? '' : path.join(req.path, '..');
-
-//     console.log('Pastas existentes:', folders);
-//     console.log(directoryPath)
-//     console.log(urlsHistory);
-//     console.log(req.originalUrl)
-
-//     res.render('index', {folders: folders, dir:newDir, emptyDirMsg, parentPath});
-//   });
-// });
-
 
 module.exports = router; 
